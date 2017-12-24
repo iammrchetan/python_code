40c184280ca368a850e589e898cafe2944f413d3
Chetan Sharma <iammrchetan@gmail.com>
2017-12-24 22:38:55 +0530

    python file added

---
+++ python.py
@@ -0,0 +1,12 @@
+name="Chetan Sharma"
+tool="Jenkins"
+dept="DevOps"
+
+print "<html>"
+print "<title>Python Page</title>"
+print "<body>"
+print '<h1 style="color:yellow;background-color:black">Admin name is '+name+'<h1>'
+print '<h2 style="color:red">Tool name is '+tool+'</h2>'
+print '<h3 style="green">Profile I want to be in is '+dept+'</h3>'
+print "</body>"
+print "</html>"
